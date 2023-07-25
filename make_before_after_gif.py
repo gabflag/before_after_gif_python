502b6ba3042da4459b69e69fa12176f7390cd6a1
Gabriel <95552879+gabflag@users.noreply.github.com>
2023-07-25 20:08:39 -0300

    Update make_before_after_gif.py

--- make_before_after_gif.py
+++ make_before_after_gif.py
@@ -1,6 +1,16 @@
 import numpy as np
 from PIL import Image
 
+def compress_image(input_path, output_path, max_size):
+    image = Image.open(input_path)
+    quality = 95 
+        
+    while True:
+        image.save(output_path, optimize=True, quality=quality)
+        if os.path.getsize(output_path) <= max_size * 1024:
+            break
+        quality -= 5
+
 
 def generate_comparative_gif(
         image1_path, image2_path, output_path, gif_duration=100, gif_delay=50):
@@ -42,8 +52,21 @@ def generate_comparative_gif(
     )
 
 
-image1_path = "after.jpg"
-image2_path = "before.jpg"
+
+input_image_path = 'after.jpg'
+output_image_path = '01_after_reduce.jpg'
+max_file_size_kb = 32
+compress_image(input_image_path, output_image_path, max_file_size_kb)
+
+input_image_path = 'before.jpg'
+output_image_path = '01_before_reduce.jpg'
+max_file_size_kb = 32
+compress_image(input_image_path, output_image_path, max_file_size_kb)
+
+
+image1_path = "01_after_reduce.jpg"
+image2_path = "01_before_reduce.jpg"
 output_gif_path = "yourgif.gif"
 
 generate_comparative_gif(image1_path, image2_path, output_gif_path)
+
