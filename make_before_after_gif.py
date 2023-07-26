dc645d5d8a013afd09ab667d4956fb492bc747bc
Gabriel <95552879+gabflag@users.noreply.github.com>
2023-07-25 21:07:19 -0300

    updated code optimizing algorithm

--- make_before_after_gif.py
+++ make_before_after_gif.py
@@ -1,14 +1,41 @@
+import os
+
 import numpy as np
 from PIL import Image
 
+
+def resize_image(background_path, new_width):
+    '''
+    Transforma a imagem com a largura especificada
+    sem perder suas dimensões e salva a imagem redimensionada no disco.
+    '''
+    background = Image.open(background_path)
+
+    width_percent = (new_width / float(background.size[0]))
+    new_height = int((float(background.size[1]) * float(width_percent)))
+
+    resized_background = background.resize(
+        (new_width, new_height), Image.LANCZOS)
+
+    resized_background.save(background_path)
+
+    return background_path
+
+
 def compress_image(input_path, output_path, max_size):
     image = Image.open(input_path)
-    quality = 95 
-        
+    quality = 95  # Qualidade inicial da imagem (0-100)
+
+    # Loop para reduzir a qualidade até que o tamanho seja 
+    # menor ou igual ao máximo permitido
     while True:
         image.save(output_path, optimize=True, quality=quality)
+
+        # Verifica o tamanho da imagem após salvar
         if os.path.getsize(output_path) <= max_size * 1024:
             break
+
+        # Reduz a qualidade em 5 unidades a cada iteração
         quality -= 5
 
 
@@ -52,7 +79,6 @@ def generate_comparative_gif(
     )
 
 
-
 input_image_path = 'after.jpg'
 output_image_path = '01_after_reduce.jpg'
 max_file_size_kb = 32
@@ -64,9 +90,12 @@ max_file_size_kb = 32
 compress_image(input_image_path, output_image_path, max_file_size_kb)
 
 
-image1_path = "01_after_reduce.jpg"
-image2_path = "01_before_reduce.jpg"
+image1_path_str = resize_image(
+    "01_after_reduce.jpg", 400)
+
+image2_path_str = resize_image(
+    "01_before_reduce.jpg", 400)
+
 output_gif_path = "yourgif.gif"
 
-generate_comparative_gif(image1_path, image2_path, output_gif_path)
-
+generate_comparative_gif(image1_path_str, image2_path_str, output_gif_path)
